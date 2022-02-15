e9d2702f7ae787e950bdf40cd5ae9cffbb8041c4
nikwok <77155696+NIKITAvinogradovs@users.noreply.github.com>
2022-02-15 08:44:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+O = int (input("Ievadi skaitļū "))
+if O%2==0 and O!=0:
+  print (O, "Paraskaitlis")
+elif O==0:
+  print("Labdien, Es esmu programma. Diemžel mans autors izdarija mani ar 0 neskaitišanu. Atvajnojiet mani neko nevaru izdarit 😭 😢")
+else:
+  print(O, "Neparaskaitlis")
